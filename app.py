9d68c728c61aaeb9183251c908b10c9c1c37ee77
Nazmul Hossen <nazmul.ict92@gmail.com>
2025-01-30 22:36:49 -0600

    Update files

--- app.py
+++ app.py
@@ -31,11 +31,14 @@ def predict():
     data = [float(x) for x in request.form.values()]  # data is a list
     
     # do scaling for only nurerical columns
-    
-    scaled_data = scaling_model.fit_transform([data])
+     # Apply normalization based on dataset description
+    data[6] /= 41  # temp (divided by 41)
+    data[7] /= 50  # atemp (divided by 50)
+    data[8] /= 100  # hum (divided by 100)
+    data[9] /= 67  # windspeed (divided by 67)
 
     # Convert data to numpy array and reshape for prediction
-    input_data = np.array(scaled_data).astype('float64').reshape(1, -1)
+    input_data = np.array(data).astype('float64').reshape(1, -1)
 
     # Make prediction
     output = bike_model.predict(input_data)[0]
